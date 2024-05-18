c9ee104ccefd3c77f339535cc34ab4d0820e2ebe
bijaydon <bijay@danpheit.com>
2024-05-18 16:09:56 +0545

    routes creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  scope :v1 do
+    # route for registration
+    devise_scope :user do
+      post 'users/sign_up', to: 'users/registrations#create'
+    end
+    # route for login
+    devise_scope :user do
+      post 'auth/sign_in', to: 'users/sessions#create'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
