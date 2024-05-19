db81951d4105338c7f2f8ff179f18d95796cfa9f
bijaydon <bijay@danpheit.com>
2024-05-19 10:56:08 +0545

    token dispatch request updated

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -323,7 +323,7 @@ Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
     jwt.dispatch_requests = [
-      ['POST', %r{^/api/v1/auth/sign_in$}]
+      ['POST', %r{^/api/v1/auth/signin$}]
     ]
     # UNCOMMENT_ME: uncoment the recovation_request and update it with
     # the method name and url to which the jwt is reinvoked
